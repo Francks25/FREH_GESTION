cf87ff6b3276d8e203a2ad768ecc4accdae07945
Franklin325 <kemchecalixte@gmail.com>
2023-01-08 23:40:23 +0100

    gestion listes d'affichage (suite).

--- app/src/main/java/com/example/frehgestion/montexte.java
+++ app/src/main/java/com/example/frehgestion/montexte.java
@@ -3,8 +3,13 @@ package com.example.frehgestion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.example.frehgestion.adapters.ItemsReferenceAdapter;
+import com.example.frehgestion.models.items_reference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class montexte extends AppCompatActivity {
 
@@ -13,11 +18,15 @@ public class montexte extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_montexte);
 
-        ListView list = (ListView)findViewById(R.id.listView1);
-        ArrayAdapter<String> tableau = new ArrayAdapter<String>(list.getContext(),R.layout.activity_montexte);
-        for(int i=0; i<40; i++) {
-            tableau.add("coucou " + i); }
-        list.setAdapter(tableau);
+        //list of items
+        List<items_reference> itemsList= new ArrayList<>();
+        itemsList.add(new items_reference("laptop", 300.0));
+        itemsList.add(new items_reference("disque Dur", 200));
+        itemsList.add(new items_reference("telephone", 150));
+
+        //getlistview
+        ListView shopListView = findViewById(R.id.l1);
+        shopListView.setAdapter(new ItemsReferenceAdapter(this, itemsList));
     }
 
 }
